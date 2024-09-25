801385a07a4685958ebda99f6979c8cd37ccb094
unknown <geovanna.sousa6@etec.sp.gov.br>
2024-09-24 22:03:31 -0300

    estilização CSS

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
   return (
 
     <View style={styles.container}>
-     <Text>Canal:{room}</Text>
+     <Text style = {styles.title}> Canal:{room}</Text>
 
      <TextInput
      placeholder='Digite sua mensagem'
@@ -37,12 +37,12 @@ export default function App() {
      onChangeText={setMessage}
      />
 
-     <Pressable onPress={sendMessage}>
-      <Text>Enviar mensagem</Text>
+     <Pressable style={styles.button} onPress={sendMessage}>
+      <Text style={styles.buttonText}>Enviar mensagem</Text>
      </Pressable>
 
-     <Text>mensagem recebida:</Text>
-     <Text>{receivedMessage || 'Nenhuma mensagem recebida'}</Text>
+     <Text style={styles.receivedMessageTitle}>mensagem recebida:</Text>
+     <Text style>{receivedMessage || 'Nenhuma mensagem recebida'}</Text>
 
     </View>
   );
@@ -51,5 +51,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor:'#f4f4f4',
+    padding: 20,
+    justifyContent:'center'
   },
+
+  title:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  input:{
+    height:50,
+    borderColor:'#ccc',
+    borderWidth:1,
+    borderRadius: 5,
+    paddingHorizontal:5,
+  },
+  button:{
+    backgroundColor: '#4CAF50',
+    paddingVertical: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom:20
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 16,
+    fontWeight:'bold'
+  },
+  receivedMessageTitle:{
+    fontSize:16,
+    fontWeight:'bold',
+    marginTop:20,
+    textAlign:'center'
+  },
+  receivedMessage:{
+    fontSize:16,
+    color: '#333',
+    marginTop:10,
+    padding:10,
+    backgroundColor: '#e8e8e8',
+    borderRadius: 5,
+    textAlign:'center'
+   }
 });
